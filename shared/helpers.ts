4f40cb91bcef7cd3432456d3ef3bc4d884988ff0
Guilherme Maneskul <guilherme@maneskul.com.br>
2023-02-08 20:01:16 -0300

    Delete shared directory

--- shared/helpers.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-const exp = global.exports;
-export const QBCore = exp['qb-core'].GetCoreObject();
-export const QBCoreFunctions = exp['qb-core'].GetCoreObject();
